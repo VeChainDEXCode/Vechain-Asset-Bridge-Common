8d1fe096374c1fbbf0d82c0939846d69e719be93
Mog.Lu <mog.lu@vechain.com>
2021-11-24 23:25:04 +0800

    add FungibleToken contract

---
+++ contracts/common/Contract_FungibleToken.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity ^0.8.0;
+
+import "./Interface_Token.sol";
+
+contract FungibleToken is IToken{
+    string public override name = "";
+    string public override symbol = "";
+    uint8 public override decimals = 0;
+    uint256 public override totalSupply = 0;
+
+    mapping (address => uint)                       public override balanceOf;
+    mapping (address => mapping (address => uint))  public override allowance;
+
+    address public bridge;
+
+    constructor(string memory _name,string memory _symbol,uint8 _decimals,address _bridge) {
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+        bridge = _bridge;
+    }
+
+    function transfer(address _to, uint256 _amount) external override returns(bool){
+        return transferFrom(msg.sender,_to,_amount);
+    }
+
+    function transferFrom(address _from, address _to, uint256 _amount) public override returns(bool){
+        require(balanceOf[_from] >= _amount, "insufficient balance");
+
+        if(_from != msg.sender && allowance[_from][msg.sender] != uint256(0)){
+            require(
+                allowance[_from][msg.sender] >= _amount,
+                "insufficient allowance"
+            );
+            allowance[_from][msg.sender] = allowance[_from][msg.sender] - _amount;
+        }
+
+        balanceOf[_from] = balanceOf[_from] - _amount;
+        balanceOf[_to] = balanceOf[_to] + _amount;
+
+        emit Transfer(_from, _to, _amount);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _amount) external override returns(bool){
+        allowance[msg.sender][_spender] = _amount;
+        emit Approval(msg.sender, _spender, _amount);
+        return true;
+    }
+}
